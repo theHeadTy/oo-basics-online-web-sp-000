f5c19fb95da912954cc69c58167553ce36fd9e97
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-08 07:16:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title = title
+  end
 
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
 
+book = Book.new('And Then There Were None')
+book.author = 'Agatha Christie'
+book.page_count = 272
+book.genre = 'Mystery'
